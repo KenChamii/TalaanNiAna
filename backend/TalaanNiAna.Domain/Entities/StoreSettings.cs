7283ac652510ce021e518bd7b0ad655a743fdf50
KenChamii <irreversiblechain@gmail.com>
2026-06-20 05:58:04 +0800

    feat(database): implement domain entities, application db context, and run initial migration

---
+++ backend/TalaanNiAna.Domain/Entities/StoreSettings.cs
@@ -0,0 +1,7 @@
+﻿public class StoreSettings
+{
+    public int Id { get; set; }
+    public string StoreName { get; set; } = string.Empty;
+    public int LowStockAlertLimit { get; set; } = 10;
+    public string SmsAlertTemplate { get; set; } = "Hi Suki, paalala lang...";
+}
