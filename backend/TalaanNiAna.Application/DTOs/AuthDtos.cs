4936868c20f83efa1e2da6b957d8415b7a330752
KenChamii <irreversiblechain@gmail.com>
2026-06-20 16:24:13 +0800

    feat(auth): implement jwt authentication, login, and user registration

---
+++ backend/TalaanNiAna.Application/DTOs/AuthDtos.cs
@@ -0,0 +1,8 @@
+﻿public record LoginDto(string Username, string Password);
+
+public record RegisterDto(
+    string Username,
+    string Password,
+    string DisplayName,
+    string StoreName
+);
